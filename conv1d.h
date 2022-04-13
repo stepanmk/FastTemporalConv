d9bc2615ad226556b9443c51a58f348ac5dedd9f
Štěpán Miklánek <63413990+stepanmk@users.noreply.github.com>
2022-04-13 17:18:59 +0200

    add conv layers

---
+++ conv1d.h
@@ -0,0 +1,206 @@
+#pragma once
+#include <Eigen/Dense>
+#include <vector>
+
+namespace FastConv
+{
+
+    template <typename T, int inSizet, int outSizet, int kernelSize, int dilation>
+    class Conv1DT
+    {
+
+        static constexpr auto memorySize = (kernelSize - 1) * dilation + 1;
+        using vecType = Eigen::Vector<T, outSizet>;
+        using filterType = Eigen::Matrix<T, inSizet, kernelSize>;
+        using memoryType = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+        using memoryPtrsType = Eigen::Vector<int, kernelSize>;
+
+    public:
+        static constexpr auto inSize = inSizet;
+        static constexpr auto outSize = outSizet;
+        
+
+        Conv1DT()
+        {
+            for (int i = 0; i < outSize; ++i)
+                weights[i] = filterType::Random();
+            inCols.setZero();
+            bias = vecType::Random();
+            reset();
+        }
+
+        void reset() {
+            memoryPtr = 0;
+            memory.resize(inSizet, memorySize);
+            memory.setZero();
+        }
+
+        void setWeights(const std::vector<std::vector<std::vector<T>>>& w)
+        {
+            for (int i = 0; i < outSize; ++i)
+                for (int k = 0; k < inSize; ++k)
+                    for (int j = 0; j < kernelSize; ++j)
+                        weights[i](k, j) = w[i][k][j];
+        }
+
+        void setBias(const std::vector<T>& b)
+        {
+            for (int i = 0; i < outSize; ++i)
+                bias(i) = b[i];
+        }
+
+        inline void forward(const T* in, T* out)
+        {
+            memory.col(memoryPtr) = Eigen::Map<const Eigen::Vector<T, inSize>>(in, inSize);
+            setColPointers(memoryColPtrs);
+
+            for (int k = 0; k < kernelSize; ++k)
+            {
+                inCols.col(k) = memory.col(memoryColPtrs(k));
+            }
+
+            for (int i = 0; i < outSize; ++i)
+                out[i] = inCols.cwiseProduct(weights[i]).sum() + bias(i);
+            
+            memoryPtr = mod(memoryPtr + 1, memorySize);
+        }
+
+    private:
+
+        memoryType memory;
+        int memoryPtr = 0;
+
+        filterType weights[outSize];
+        vecType bias;
+
+        memoryPtrsType memoryColPtrs;
+        filterType inCols;
+
+        int mod(int a, int b)
+        {
+            int r = a % b;
+            return r < 0 ? r + b : r;
+        }
+
+        inline void setColPointers(memoryPtrsType& ptr)
+        {
+            for (int i = 0; i < kernelSize; ++i)
+                ptr(i) = mod((memoryPtr - i * dilation), memorySize);
+        }
+    };
+
+    template <typename T>
+    class Conv1D
+    {
+        using vecType = Eigen::Vector<T, Eigen::Dynamic>;
+        using filterType = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+        using memoryType = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>;
+        using memoryPtrsType = Eigen::VectorXi;
+
+    public:
+
+        size_t inSize;
+        size_t outSize;
+        size_t kernelSize;
+        size_t memorySize;
+        int dilation;
+
+        Conv1D(size_t inSize, size_t outSize, size_t kernelSize, int dilation) :
+            inSize(inSize),
+            outSize(outSize),
+            kernelSize(kernelSize),
+            memorySize((kernelSize - 1)* dilation + 1),
+            dilation(dilation)
+        {
+            weights.resize(outSize);
+            for (int i = 0; i < outSize; ++i)
+                weights[i] = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Random(inSize, kernelSize);
+            bias = Eigen::Vector<T, Eigen::Dynamic>::Random(outSize);
+
+            memory = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Random(inSize, memorySize);
+            memoryColPtrs = Eigen::VectorXi::Zero(kernelSize);
+            inCols = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Zero(inSize, kernelSize);
+            outs.resize(outSize);
+            outs.setZero();
+        }
+
+        /** Resets the layer state. */
+        void reset(size_t newInSize, size_t newOutSize, size_t newKernelSize, int newDilation)
+        {
+            inSize = newInSize;
+            outSize = newOutSize;
+            kernelSize = newKernelSize;
+            dilation = newDilation;
+            memorySize = (kernelSize - 1) * dilation + 1;
+
+            weights.resize(outSize);
+            for (int i = 0; i < outSize; ++i)
+                weights[i] = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Random(inSize, kernelSize);
+
+            bias = Eigen::Vector<T, Eigen::Dynamic>::Random(outSize);
+
+            memory = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Zero(inSize, memorySize);
+            memoryColPtrs = Eigen::VectorXi::Zero(kernelSize);
+            
+            inCols = Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>::Zero(inSize, kernelSize);
+            
+            outs.resize(outSize);
+            outs.setZero();
+            memoryPtr = 0;
+        }
+
+        void setWeights(const std::vector<std::vector<std::vector<T>>>& w)
+        {
+            for (int i = 0; i < outSize; ++i)
+                for (int k = 0; k < inSize; ++k)
+                    for (int j = 0; j < kernelSize; ++j)
+                        weights[i](k, j) = w[i][k][j];
+        }
+
+        void setBias(const std::vector<T>& b)
+        {
+            for (int i = 0; i < outSize; ++i)
+                bias(i) = b[i];
+        }
+
+        inline void forward(const T* in, T* out)
+        {
+            memory.col(memoryPtr) = Eigen::Map<const Eigen::Vector<T, Eigen::Dynamic>>(in, inSize);
+            setColPointers(memoryColPtrs);
+
+            for (int k = 0; k < kernelSize; ++k)
+            {
+                inCols.col(k) = memory.col(memoryColPtrs(k));
+            }
+
+            for (int i = 0; i < outSize; ++i)
+                out[i] = inCols.cwiseProduct(weights[i]).sum() + bias(i);
+
+            memoryPtr = mod(memoryPtr + 1, memorySize);
+        }
+
+    private:
+
+        memoryType memory;
+        int memoryPtr = 0;
+
+        std::vector<Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic>> weights;
+        vecType bias;
+
+        memoryPtrsType memoryColPtrs;
+        filterType inCols;
+
+        int mod(int a, int b)
+        {
+            int r = a % b;
+            return r < 0 ? r + b : r;
+        }
+
+        inline void setColPointers(memoryPtrsType& ptr)
+        {
+            for (int i = 0; i < kernelSize; ++i)
+                ptr(i) = mod((memoryPtr - i * dilation), memorySize);
+        }
+    };
+
+} // FastConv
---
+++ conv1d_gated.h
@@ -0,0 +1,119 @@
+#pragma once
+#include "fast_conv1d.h"
+
+
+namespace FastConv
+{
+
+    template <typename T, int inSizet, int outSizet, int kernelSize, int dilation>
+    class Conv1DTGated
+    {
+        using vecType = Eigen::Vector<T, outSizet>;
+    public:
+        static constexpr auto inSize = inSizet;
+        static constexpr auto outSize = outSizet;
+        static constexpr auto innerSize = inSizet * 2;
+        static constexpr auto memorySize = (kernelSize - 1) * dilation + 1;
+
+        Conv1DTGated()
+        {
+            reset();
+        }
+
+        void reset() 
+        {
+            innerOuts.resize(innerSize);
+        }
+
+        Conv1DT<T, inSize, innerSize, kernelSize, dilation> inConv;
+        Conv1DT<T, inSize, outSize, 1, 1> outConv;
+
+        std::vector<T> innerOuts;
+        vecType tanhOuts;
+        vecType sigmOuts;
+        
+        vecType outs;
+
+        inline void forward(const T* in, T* out, T* skip, int skipIdx)
+        {
+            inConv.forward(in, innerOuts.data());
+            tanhOuts = Eigen::Map<Eigen::Vector<T, inSize>>(innerOuts.data(), outSize);
+            sigmOuts = Eigen::Map<Eigen::Vector<T, inSize>>(innerOuts.data() + outSize, outSize);
+            
+            tanhOuts = tanhOuts.array().tanh();
+            sigmOuts = 1.0f / (1.0f + (-sigmOuts.array().exp()));
+            
+            outs = tanhOuts.cwiseProduct(sigmOuts);
+            
+            std::copy(outs.data(), outs.data() + outSize, skip + skipIdx);
+
+            outConv.forward(outs.data(), outs.data());
+            outs += Eigen::Map<const Eigen::Vector<T, outSize>>(in, outSize);            
+            
+            out = outs.data();
+            //std::copy(outs.data(), outs.data() + outSize, out);
+        }
+    };
+
+
+    template <typename T>
+    class Conv1DGated
+    {
+        using vecType = Eigen::Vector<T, Eigen::Dynamic>;
+    public:
+
+        Conv1DGated(size_t inSize, size_t outSize, size_t kernelSize, int dilation) :
+            inSize(inSize),
+            outSize(outSize),
+            kernelSize(kernelSize),
+            innerSize(outSize * 2),
+            dilation(dilation)
+        {
+            reset();
+        }
+
+        void reset()
+        {
+            innerOuts.resize(innerSize);
+            inConv.reset(inSize, innerSize, kernelSize, dilation);
+            outConv.reset(inSize, outSize, 1, 1);
+            tanhOuts.resize(outSize);
+            sigmOuts.resize(outSize);
+            outs.resize(outSize);
+        }
+
+        Conv1D<T> inConv = Conv1D<T>(1, 1, 1, 1);
+        Conv1D<T> outConv = Conv1D<T>(1, 1, 1, 1);
+
+        size_t inSize;
+        size_t outSize;
+        size_t kernelSize;
+        size_t innerSize;
+        int dilation;
+
+        std::vector<T> innerOuts;
+        vecType tanhOuts;
+        vecType sigmOuts;
+        vecType outs;
+
+        inline void forward(const T* in, T* out, T* skip, int skipIdx)
+        {
+            inConv.forward(in, innerOuts.data());
+            tanhOuts = Eigen::Map<Eigen::Vector<T, Eigen::Dynamic>>(innerOuts.data(), outSize);
+            sigmOuts = Eigen::Map<Eigen::Vector<T, Eigen::Dynamic>>(innerOuts.data() + outSize, outSize);
+
+            tanhOuts = tanhOuts.array().tanh();
+            sigmOuts = 1.0f / (1.0f + (-sigmOuts.array().exp()));
+
+            outs = tanhOuts.cwiseProduct(sigmOuts);
+            std::copy(outs.data(), outs.data() + outSize, skip + skipIdx);
+
+            outConv.forward(outs.data(), outs.data());
+            outs += Eigen::Map<const Eigen::Vector<T, Eigen::Dynamic>>(in, outSize);
+
+            out = outs.data();
+            //std::copy(outs.data(), outs.data() + outSize, out);
+        }
+    };
+
+} // FastConv
